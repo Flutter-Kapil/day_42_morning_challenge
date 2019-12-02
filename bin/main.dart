558a4b1a191554aea05f273ed0a529527575b08d
kapil jhajhria <jhajhria44@gmail.com>
2019-12-02 10:48:35 +0530

    completed with 2 test cases

--- bin/main.dart
+++ bin/main.dart
@@ -9,6 +9,44 @@
 //  95% of the numbers are less than or equal to 22
 //  99% of the numbers are less than or equal to 22
 //  100% of the numbers are less than or equal to 22
+List<int> percentile(List<int> numbers) {
+  numbers.sort();
+  print(numbers);
+  int lengthOfList = numbers.length;
+  print(
+      '25% of the numbers are less than or equal to ${numbers[getPercentagePosition(lengthOfList, 25)]}');
+  print(
+      '50% of the numbers are less thanequal to ${numbers[getPercentagePosition(lengthOfList, 50)]}');
+  print(
+      '75% of the numbers are less than equal to ${numbers[getPercentagePosition(lengthOfList, 75)]}');
+  print(
+      '90% of the numbers are less than equal to ${numbers[getPercentagePosition(lengthOfList, 90)]}');
+  print(
+      '95% of the numbers are less than equal to ${numbers[getPercentagePosition(lengthOfList, 95)]}');
+  print(
+      '99% of the numbers are less than equal to ${numbers[getPercentagePosition(lengthOfList, 99)]}');
+  print(
+      '100% of the numbers are less than equal to ${numbers[getPercentagePosition(lengthOfList, 100)]}');
+  List<int> testList = [
+    numbers[getPercentagePosition(lengthOfList, 25)],
+    numbers[getPercentagePosition(lengthOfList, 50)],
+    numbers[getPercentagePosition(lengthOfList, 75)],
+    numbers[getPercentagePosition(lengthOfList, 90)],
+    numbers[getPercentagePosition(lengthOfList, 95)],
+    numbers[getPercentagePosition(lengthOfList, 99)],
+    numbers[getPercentagePosition(lengthOfList, 100)]
+  ];
+  return testList;
+}
+
+int getPercentagePosition(int length, int percentage) {
+  int x = ((percentage / 100) * length).ceil();
+    return x-1;
+}
 
 main() {
+print(getPercentagePosition(90, 12));
+print(getPercentagePosition(5, 75));
+print(getPercentagePosition(5,25));
+print(getPercentagePosition(13,75));
 }
--- test/Day_42_Morning_Challenge_test.dart
+++ test/Day_42_Morning_Challenge_test.dart
@@ -1,6 +1,9 @@
 import 'package:test/test.dart';
+import '../bin/main.dart';
 
 void main() {
   test('Test Case 1', () {
+    expect(percentile([1,2,3,4,5]), [2,3,4,5,5,5,5]);
+    expect(percentile([8, 6, 6, 20, 9, 1, 12, 16, 3, 16, 22, 2]), [3,8,16,20,22,22,22]);
   });
 }
