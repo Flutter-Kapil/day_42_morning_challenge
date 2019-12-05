e314a0b0ce40648c7ff403db4bc5e64468842773
kapil jhajhria <jhajhria44@gmail.com>
2019-12-05 12:17:51 +0530

    completed

--- bin/main.dart
+++ bin/main.dart
@@ -12,30 +12,14 @@
 List<int> percentile(List<int> numbers) {
   numbers.sort();
   print(numbers);
+  List<int> percentage = [25,50,75,90,95,99,100];
   int lengthOfList = numbers.length;
-  print(
-      '25% of the numbers are less than or equal to ${numbers[getPercentagePosition(lengthOfList, 25)]}');
-  print(
-      '50% of the numbers are less thanequal to ${numbers[getPercentagePosition(lengthOfList, 50)]}');
-  print(
-      '75% of the numbers are less than equal to ${numbers[getPercentagePosition(lengthOfList, 75)]}');
-  print(
-      '90% of the numbers are less than equal to ${numbers[getPercentagePosition(lengthOfList, 90)]}');
-  print(
-      '95% of the numbers are less than equal to ${numbers[getPercentagePosition(lengthOfList, 95)]}');
-  print(
-      '99% of the numbers are less than equal to ${numbers[getPercentagePosition(lengthOfList, 99)]}');
-  print(
-      '100% of the numbers are less than equal to ${numbers[getPercentagePosition(lengthOfList, 100)]}');
-  List<int> testList = [
-    numbers[getPercentagePosition(lengthOfList, 25)],
-    numbers[getPercentagePosition(lengthOfList, 50)],
-    numbers[getPercentagePosition(lengthOfList, 75)],
-    numbers[getPercentagePosition(lengthOfList, 90)],
-    numbers[getPercentagePosition(lengthOfList, 95)],
-    numbers[getPercentagePosition(lengthOfList, 99)],
-    numbers[getPercentagePosition(lengthOfList, 100)]
-  ];
+  List<int> testList = [];
+  percentage.forEach((percent) {
+    int number = numbers[getPercentagePosition(lengthOfList, percent)];
+    print('$percent% of the numbers less than or equal to $number');
+    testList.add(number);
+  });
   return testList;
 }
 
